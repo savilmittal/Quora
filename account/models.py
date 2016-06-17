366addee71414b6f8777a5a3a6f2d3652236fd7a
Savil Mittal <savilmittal1996@gmail.com>
2016-06-18 01:18:54 +0530

    app registered to admin

--- account/models.py
+++ account/models.py
@@ -4,5 +4,8 @@ from django.contrib.auth.models import AbstractUser
 class MyUser(AbstractUser):
 	contact=models.CharField(max_length=10,null=True)
 
+	def __str__(self):
+		return self.username
+
 
 
--- question/models.py
+++ question/models.py
@@ -3,9 +3,12 @@ from account.models import MyUser
 
 # Create your models here.
 class Question(models.Model):
-	title=models.CharField(max_length=50,default='')
-	text=models.CharField(max_length=1000,default='')
-	created_on=models.DateTimeField(auto_now_add=True)
-	created_by=models.ForeignKey(MyUser,related_name = 'questions_created')
-	upvoted_by=models.ManyToManyField(MyUser,related_name = 'questions_upvoted')
+    title=models.CharField(max_length=50,default='')
+    text=models.CharField(max_length=1000,default='')
+    created_on=models.DateTimeField(auto_now_add=True)
+    created_by=models.ForeignKey(MyUser,related_name = 'questions_created')
+    upvoted_by=models.ManyToManyField(MyUser,related_name = 'questions_upvoted',null=True)
+    def __str__(self):
+	    return self.title
+
 
